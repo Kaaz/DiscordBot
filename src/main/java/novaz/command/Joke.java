8df26668a8a8466f4b642157dc1df10993e22214
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2016-08-10 16:12:38 +0200

    !joke command :) what a joke huehuehue

---
+++ src/main/java/novaz/command/Joke.java
@@ -0,0 +1,53 @@
+package novaz.command;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import novaz.core.AbstractCommand;
+import novaz.handler.TextHandler;
+import novaz.main.NovaBot;
+import org.apache.commons.lang3.StringEscapeUtils;
+import sx.blah.discord.handle.obj.IUser;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Gemaakt op 10-8-2016
+ */
+public class Joke extends AbstractCommand {
+	public Joke(NovaBot b) {
+		super(b);
+		setCmd("joke");
+	}
+
+	@Override
+	public String execute(String[] args, IUser author) {
+		boolean first = true;
+		String ret = "";
+		String joketxt = getJokeFromWeb(author.getName());
+		if (joketxt != null) {
+			return StringEscapeUtils.unescapeHtml4(joketxt.replace(author.getName(), "<@"+author.getID()+">"));
+		}
+		return TextHandler.get("command_joke_not_today");
+	}
+
+	public static String getJokeFromWeb(String username) {
+		try {
+			URL loginurl = new URL("http://api.icndb.com/jokes/random?firstName=&lastName=" + username);
+			URLConnection yc = loginurl.openConnection();
+			yc.setConnectTimeout(10 * 1000);
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(
+							yc.getInputStream()));
+			String inputLine = in.readLine();
+			JsonParser parser = new JsonParser();
+			JsonObject array = parser.parse(inputLine).getAsJsonObject();
+			return array.get("value").getAsJsonObject().get("joke").getAsString();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return null;
+	}
+}
