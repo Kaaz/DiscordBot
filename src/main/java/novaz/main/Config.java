990b60bb9158d1af8327596ac8bbb34cd12a7c8d
Kaaz <maik_wezinkhof@hotmail.com>
2016-09-17 12:39:38 +0200

    init hearthstone

--- src/main/java/novaz/main/Config.java
+++ src/main/java/novaz/main/Config.java
@@ -76,6 +76,10 @@ public class Config {
 	@ConfigurationOption
 	public static boolean MODULE_POE_ENABLED = true;
 
+	//enable hearthstone globally
+	@ConfigurationOption
+	public static boolean MODULE_HEARTHSTONE_ENABLED = true;
+
 	//enable music globally
 	@ConfigurationOption
 	public static boolean MODULE_MUSIC_ENABLED = true;
