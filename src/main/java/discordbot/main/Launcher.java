4e974f2b1c01ac24695785f81f0510e3fe681523
Kaaz <maik_wezinkhof@hotmail.com>
2016-09-26 23:24:24 +0200

    quick/dirty dnd hack

--- src/main/java/discordbot/main/Launcher.java
+++ src/main/java/discordbot/main/Launcher.java
@@ -8,11 +8,18 @@ import discordbot.db.model.OMusic;
 import discordbot.db.table.TMusic;
 import discordbot.threads.ServiceHandlerThread;
 import discordbot.util.YTUtil;
+import sun.reflect.ConstructorAccessor;
+import sun.reflect.FieldAccessor;
+import sun.reflect.ReflectionFactory;
+import sx.blah.discord.handle.obj.Presences;
 import sx.blah.discord.util.DiscordException;
 
 import java.io.File;
-import java.util.Properties;
-import java.util.Random;
+import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 public class Launcher {
 	public static boolean killAllThreads = false;
@@ -28,6 +35,7 @@ public class Launcher {
 		Properties props = new Properties();
 		props.load(Launcher.class.getClassLoader().getResourceAsStream("version.properties"));
 		Launcher.version = ProgramVersion.fromString(String.valueOf(props.getOrDefault("version", "1")));
+		addEnum(Presences.class, "DND");
 		DiscordBot.LOGGER.info("Started with version: " + Launcher.version);
 		if (Config.BOT_ENABLED) {
 			DiscordBot nb = null;
@@ -76,5 +84,114 @@ public class Launcher {
 			TMusic.update(rec);
 		}
 	}
+	private static ReflectionFactory reflectionFactory = ReflectionFactory.getReflectionFactory();
 
+	private static void setFailsafeFieldValue(Field field, Object target, Object value) throws NoSuchFieldException,
+			IllegalAccessException {
+
+		// let's make the field accessible
+		field.setAccessible(true);
+
+		// next we change the modifier in the Field instance to
+		// not be final anymore, thus tricking reflection into
+		// letting us modify the static final field
+		Field modifiersField = Field.class.getDeclaredField("modifiers");
+		modifiersField.setAccessible(true);
+		int modifiers = modifiersField.getInt(field);
+
+		// blank out the final bit in the modifiers int
+		modifiers &= ~Modifier.FINAL;
+		modifiersField.setInt(field, modifiers);
+
+		FieldAccessor fa = reflectionFactory.newFieldAccessor(field, false);
+		fa.set(target, value);
+	}
+
+	private static void blankField(Class<?> enumClass, String fieldName) throws NoSuchFieldException,
+			IllegalAccessException {
+		for (Field field : Class.class.getDeclaredFields()) {
+			if (field.getName().contains(fieldName)) {
+				AccessibleObject.setAccessible(new Field[] { field }, true);
+				setFailsafeFieldValue(field, enumClass, null);
+				break;
+			}
+		}
+	}
+
+	private static void cleanEnumCache(Class<?> enumClass) throws NoSuchFieldException, IllegalAccessException {
+		blankField(enumClass, "enumConstantDirectory"); // Sun (Oracle?!?) JDK 1.5/6
+		blankField(enumClass, "enumConstants"); // IBM JDK
+	}
+
+	private static ConstructorAccessor getConstructorAccessor(Class<?> enumClass, Class<?>[] additionalParameterTypes)
+			throws NoSuchMethodException {
+		Class<?>[] parameterTypes = new Class[additionalParameterTypes.length + 2];
+		parameterTypes[0] = String.class;
+		parameterTypes[1] = int.class;
+		System.arraycopy(additionalParameterTypes, 0, parameterTypes, 2, additionalParameterTypes.length);
+		return reflectionFactory.newConstructorAccessor(enumClass.getDeclaredConstructor(parameterTypes));
+	}
+
+	private static Object makeEnum(Class<?> enumClass, String value, int ordinal, Class<?>[] additionalTypes,
+								   Object[] additionalValues) throws Exception {
+		Object[] parms = new Object[additionalValues.length + 2];
+		parms[0] = value;
+		parms[1] = Integer.valueOf(ordinal);
+		System.arraycopy(additionalValues, 0, parms, 2, additionalValues.length);
+		return enumClass.cast(getConstructorAccessor(enumClass, additionalTypes).newInstance(parms));
+	}
+
+	/**
+	 * Add an enum instance to the enum class given as argument
+	 *
+	 * @param <T> the type of the enum (implicit)
+	 * @param enumType the class of the enum to be modified
+	 * @param enumName the name of the new enum instance to be added to the class.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T extends Enum<?>> void addEnum(Class<T> enumType, String enumName) {
+
+		// 0. Sanity checks
+		if (!Enum.class.isAssignableFrom(enumType)) {
+			throw new RuntimeException("class " + enumType + " is not an instance of Enum");
+		}
+
+		// 1. Lookup "$VALUES" holder in enum class and get previous enum instances
+		Field valuesField = null;
+		Field[] fields = Presences.class.getDeclaredFields();
+		for (Field field : fields) {
+			if (field.getName().contains("$VALUES")) {
+				valuesField = field;
+				break;
+			}
+		}
+		AccessibleObject.setAccessible(new Field[] { valuesField }, true);
+
+		try {
+
+			// 2. Copy it
+			T[] previousValues = (T[]) valuesField.get(enumType);
+			List<T> values = new ArrayList<T>(Arrays.asList(previousValues));
+
+			// 3. build new enum
+			T newValue = (T) makeEnum(enumType, // The target enum class
+					enumName, // THE NEW ENUM INSTANCE TO BE DYNAMICALLY ADDED
+					values.size(),
+					new Class<?>[] {}, // could be used to pass values to the enum constuctor if needed
+					new Object[] {}); // could be used to pass values to the enum constuctor if needed
+
+			// 4. add new value
+			values.add(newValue);
+
+			// 5. Set new values field
+			setFailsafeFieldValue(valuesField, null, values.toArray((T[]) Array.newInstance(enumType, 0)));
+
+			// 6. Clean enum cache
+			cleanEnumCache(enumType);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
 }
