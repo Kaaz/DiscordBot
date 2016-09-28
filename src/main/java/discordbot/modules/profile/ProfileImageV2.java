2c3dfe3c7b19c6049542cbdb8648b384ce9fe29f
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2016-09-28 09:46:32 +0200

    made by a bit less intrusive

--- src/main/java/discordbot/modules/profile/ProfileImageV2.java
+++ src/main/java/discordbot/modules/profile/ProfileImageV2.java
@@ -24,7 +24,7 @@ public class ProfileImageV2 extends ProfileImage {
 
 	public File getProfileImage() throws IOException {
 		Font defaultFont = new Font("Helvetica", Font.BOLD + Font.ITALIC, 28);
-		Font score = new Font("Helvetica", Font.BOLD + Font.ITALIC, 24);
+		Font score = new Font("Helvetica", Font.BOLD, 24);
 		Font creditFont = new Font("comissans", Font.ITALIC, 20);
 		BufferedImage result = new BufferedImage(
 				320, 265,
@@ -40,11 +40,11 @@ public class ProfileImageV2 extends ProfileImage {
 		g.drawImage(backgroundImage, 0, 0, 320, 265, 0, 0, 320, 265, null);
 
 		GfxUtil.addText(getUser().getName(), defaultFont, 148, 105, g, Color.white);
-		GfxUtil.addRightText("made by Emily", creditFont, 315, 202, g, new Color(0x262626));
+		GfxUtil.addRightText("made by Emily", creditFont, 318, 199, g, new Color(0x3A3A38));
 
-		GfxUtil.addCenterText("" + rng.nextInt(100), defaultFont, 31, 246, g, new Color(0x5c7e32));
-		GfxUtil.addCenterText("" + rng.nextInt(100), defaultFont, 134, 246, g, new Color(0x5c7e32));
-		GfxUtil.addCenterText("" + rng.nextInt(100), defaultFont, 237, 246, g, new Color(0x5c7e32));
+		GfxUtil.addCenterText("" + rng.nextInt(100), score, 31, 246, g, new Color(0x5c7e32));
+		GfxUtil.addCenterText("" + rng.nextInt(100), score, 134, 246, g, new Color(0x5c7e32));
+		GfxUtil.addCenterText("" + rng.nextInt(100), score, 237, 246, g, new Color(0x5c7e32));
 		File file = new File("profile_v2_" + getUser().getID() + ".png");
 		ImageIO.write(result, "png", file);
 		return file;
