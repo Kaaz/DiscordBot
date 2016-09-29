5bb0d03f4106843c52b71ae3a6abe2f0331c6c85
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2016-09-29 16:13:50 +0200

    Oops! reversed the owner check

--- src/main/java/discordbot/command/fun/TagCommand.java
+++ src/main/java/discordbot/command/fun/TagCommand.java
@@ -59,7 +59,7 @@ public class TagCommand extends AbstractCommand {
 		}
 		OTag tag = TTag.findBy(channel.getGuild().getID(), args[0]);
 		if (args.length > 1) {
-			if (tag.id > 0 && tag.userId == TUser.getCachedId(author.getID())) {
+			if (tag.id > 0 && tag.userId != TUser.getCachedId(author.getID())) {
 
 				return Template.get("command_tag_only_creator_can_edit");
 			}
