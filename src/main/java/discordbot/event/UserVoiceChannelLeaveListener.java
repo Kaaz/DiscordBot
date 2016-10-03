f83521078013f9d82e1bb3186b9db60d26d3427f
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2016-10-03 10:14:13 +0200

    leave voicechannel+stop music if a user leaves and only bots remain in the channel

---
+++ src/main/java/discordbot/event/UserVoiceChannelLeaveListener.java
@@ -0,0 +1,48 @@
+package discordbot.event;
+
+import discordbot.core.AbstractEventListener;
+import discordbot.handler.MusicPlayerHandler;
+import discordbot.main.DiscordBot;
+import sx.blah.discord.handle.impl.events.UserVoiceChannelLeaveEvent;
+import sx.blah.discord.handle.obj.IUser;
+import sx.blah.discord.handle.obj.IVoiceChannel;
+
+/**
+ * If a user leaves a voice channel
+ */
+public class UserVoiceChannelLeaveListener extends AbstractEventListener<UserVoiceChannelLeaveEvent> {
+	public UserVoiceChannelLeaveListener(DiscordBot discordBot) {
+		super(discordBot);
+	}
+
+	@Override
+	public boolean listenerIsActivated() {
+		return true;
+	}
+
+	@Override
+	public void handle(UserVoiceChannelLeaveEvent event) {
+		IVoiceChannel channel = event.getChannel();
+		IVoiceChannel connectedVoice = null;
+		for (IVoiceChannel voiceChannel : discordBot.instance.getConnectedVoiceChannels()) {
+			if (voiceChannel.getID().equals(channel.getID())) {
+				connectedVoice = voiceChannel;
+				break;
+			}
+		}
+		if (connectedVoice != null) {
+			boolean shouldLeave = true;
+			for (IUser user : connectedVoice.getConnectedUsers()) {
+				if (!user.isBot()) {
+					shouldLeave = false;
+					break;
+				}
+			}
+			if (shouldLeave) {
+				MusicPlayerHandler.getAudioPlayerForGuild(channel.getGuild(), discordBot).stopMusic();
+				connectedVoice.leave();
+			}
+		}
+	}
+
+}
