4892be7e6859078d3c0774033d79a7434559ea4f
Kaaz <maik_wezinkhof@hotmail.com>
2016-10-08 13:24:55 +0200

    use configured prefix in the message, formatting + joinServer -> GuildJoin

--- src/main/java/discordbot/event/GuildJoinListener.java
+++ src/main/java/discordbot/event/GuildJoinListener.java
@@ -5,8 +5,8 @@ import discordbot.db.model.OServer;
 import discordbot.db.model.OUser;
 import discordbot.db.table.TServers;
 import discordbot.db.table.TUser;
-import discordbot.guildsettings.DefaultGuildSettings;
 import discordbot.guildsettings.defaults.SettingCommandPrefix;
+import discordbot.handler.GuildSettings;
 import discordbot.main.Config;
 import discordbot.main.DiscordBot;
 import sx.blah.discord.handle.impl.events.GuildCreateEvent;
@@ -18,8 +18,8 @@ import sx.blah.discord.handle.obj.Permissions;
 /**
  * Whenever the bot joins a discord server
  */
-public class JoinServerListener extends AbstractEventListener<GuildCreateEvent> {
-	public JoinServerListener(DiscordBot discordBot) {
+public class GuildJoinListener extends AbstractEventListener<GuildCreateEvent> {
+	public GuildJoinListener(DiscordBot discordBot) {
 		super(discordBot);
 	}
 
@@ -42,21 +42,21 @@ public class JoinServerListener extends AbstractEventListener<GuildCreateEvent> 
 		server.discord_id = guild.getID();
 		server.name = guild.getName();
 		server.owner = user.id;
-		String cmdPre = DefaultGuildSettings.getDefault(SettingCommandPrefix.class);
+		String cmdPre = GuildSettings.get(guild).getOrDefault(SettingCommandPrefix.class);
 		if (server.active == 0) {
 			discordBot.out.sendMessageToCreator(String.format("[**event**] [**guild**] I have just **joined** **%s** (discord-id = %s)", guild.getName(), guild.getID()));
-			server.active = 1;
 
 			String message = "Thanks for adding me to your guild!" + Config.EOL +
-					"To see what I can do you can type the command `" + cmdPre + "`." + Config.EOL +
+					"To see what I can do you can type the command `" + cmdPre + "help`." + Config.EOL +
 					"Most of my features are opt-in, which means that you'll have to enable them first. Admins can use `" + cmdPre + "config` to change my settings." + Config.EOL +
-					"If you need help or would like to give feedback, feel free to let me know on either !discord or !github";
+					"If you need help or would like to give feedback, feel free to let me know on either `" + cmdPre + "discord` or `" + cmdPre + "github`";
 			IChannel channel = guild.getChannels().get(0);
 			if (channel != null && channel.getModifiedPermissions(discordBot.client.getOurUser()).contains(Permissions.SEND_MESSAGES)) {
 				discordBot.out.sendMessage(channel, message);
 			} else {
 				discordBot.out.sendPrivateMessage(owner, message);
 			}
+			server.active = 1;
 		}
 		TServers.update(server);
 		DiscordBot.LOGGER.info("[event] JOINED SERVER! " + guild.getName());
