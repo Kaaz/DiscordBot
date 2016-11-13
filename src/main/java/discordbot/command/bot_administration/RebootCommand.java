7d5e76b4072e5dca8bc386ca708a13ecadf9b107
Kaaz <maik_wezinkhof@hotmail.com>
2016-11-13 11:41:33 +0100

    reboot with 0 arguments is possible again :D

--- src/main/java/discordbot/command/bot_administration/RebootCommand.java
+++ src/main/java/discordbot/command/bot_administration/RebootCommand.java
@@ -49,7 +49,7 @@ public class RebootCommand extends AbstractCommand {
 				bot.out.sendAsyncMessage(channel, Template.get("command_reboot_update"), message -> {
 					Launcher.stop(ExitCode.UPDATE);
 				});
-			} else if (args.length > 0 && args[0].equals("forceupdate") || args[0].equals("fursupdate")) {
+			} else if (args.length > 0 && (args[0].equals("forceupdate") || args[0].equals("fursupdate"))) {
 				bot.out.sendAsyncMessage(channel, Template.get("command_reboot_forceupdate"), message -> {
 					Launcher.stop(ExitCode.UPDATE);
 				});
