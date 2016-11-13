a9a86850f27f953b9b15938bda6e6a4d78c5f3f4
Kaaz <maik_wezinkhof@hotmail.com>
2016-11-13 11:36:56 +0100

    windows and filenames p3

---
+++ src/main/java/discordbot/db/model/OBotPlayingOn3.java
@@ -0,0 +1,11 @@
+package discordbot.db.model;
+
+import discordbot.db.AbstractModel;
+
+/**
+ * Created on 5-9-2016
+ */
+public class OBotPlayingOn3 extends AbstractModel {
+	public String guildId = "";
+	public String channelId = "";
+}
