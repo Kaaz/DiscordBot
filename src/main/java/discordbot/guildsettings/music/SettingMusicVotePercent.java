d1c9e8ba11a99e8129cb988ad356c8b7a1f43bc2
Kaaz <maik_wezinkhof@hotmail.com>
2016-11-13 11:32:40 +0100

    music setting + bump to 1.8.8

---
+++ src/main/java/discordbot/guildsettings/music/SettingMusicVotePercent.java
@@ -0,0 +1,39 @@
+package discordbot.guildsettings.music;
+
+import discordbot.guildsettings.AbstractGuildSetting;
+
+
+public class SettingMusicVotePercent extends AbstractGuildSetting {
+	@Override
+	public String getKey() {
+		return "music_vote_percent";
+	}
+
+	@Override
+	public String getDefault() {
+		return "1";
+	}
+
+	@Override
+	public String[] getDescription() {
+		return new String[]{
+				"Percentage of users (rounded down) required to skip the currently playing track",
+				"",
+				"eg; when set to 25, and 5 listeners it would require 2 users to vote skip ",
+				"",
+				"Accepts a value between 1 and 100"
+		};
+	}
+
+	@Override
+	public boolean isValidValue(String input) {
+		try {
+			int vol = Integer.parseInt(input);
+			if (vol >= 0 && vol <= 100) {
+				return true;
+			}
+		} catch (Exception ignored) {
+		}
+		return false;
+	}
+}
