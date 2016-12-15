b199720bccd98ef3ca4438b2565e0c021a93e24c
Kaaz <maik_wezinkhof@hotmail.com>
2016-12-15 09:14:23 +0100

    mark ready on reconnect

--- src/main/java/discordbot/event/JDAReadyEvent.java
+++ src/main/java/discordbot/event/JDAReadyEvent.java
@@ -2,6 +2,7 @@ package discordbot.event;
 
 import discordbot.main.DiscordBot;
 import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.events.ReconnectedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class JDAReadyEvent extends ListenerAdapter {
@@ -16,5 +17,10 @@ public class JDAReadyEvent extends ListenerAdapter {
 	public void onReady(ReadyEvent event) {
 		discordBot.markReady();
 		System.out.println("[event] Bot is ready!");
+	}
+
+	@Override
+	public void onReconnect(ReconnectedEvent event) {
+		discordBot.markReady();
 	}
 }
