68fa6253aa094b67818091a2ae1728eae394ab25
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2016-12-19 17:10:26 +0100

    !poll command, vote with reactions

--- src/main/java/discordbot/command/fun/PollCommand.java
+++ src/main/java/discordbot/command/fun/PollCommand.java
@@ -3,9 +3,15 @@ package discordbot.command.fun;
 import discordbot.command.CommandVisibility;
 import discordbot.core.AbstractCommand;
 import discordbot.handler.Template;
+import discordbot.main.Config;
 import discordbot.main.DiscordBot;
+import discordbot.util.Misc;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
+import net.dv8tion.jda.core.utils.PermissionUtil;
 
 public class PollCommand extends AbstractCommand {
 	private static final int MAX_DURATION_IN_MINUTES = 30;
@@ -33,7 +39,7 @@ public class PollCommand extends AbstractCommand {
 	public String[] getUsage() {
 		return new String[]{
 				"poll          //status of active poll ",
-				"poll create <question> ;<duration in minutes>;<option1>;<option2>;<etc.> ",
+				"poll create <question> ;<option1>;<option2>;<etc.>   (max 8)",
 				"              //creates a poll for the duration",
 				"poll 1-9      //vote on the options",
 		};
@@ -51,6 +57,10 @@ public class PollCommand extends AbstractCommand {
 
 	@Override
 	public String execute(DiscordBot bot, String[] args, MessageChannel channel, User author) {
+		Guild guild = ((TextChannel) channel).getGuild();
+		if (!PermissionUtil.checkPermission((TextChannel) channel, guild.getSelfMember(), Permission.MESSAGE_ADD_REACTION)) {
+			return "need permission to add reactions";
+		}
 		if (args.length == 0) {
 			return "show overview";
 		}
@@ -61,19 +71,23 @@ public class PollCommand extends AbstractCommand {
 				argument += " " + args[i];
 			}
 			String[] split = argument.split(";");
-			if (split.length > 4) {
+			if (split.length < 3) {
 				return "Invalid usage! Need at least 2 options " + getUsage()[1];
 			}
 			if (split[0].trim().length() < 3) {
 				return Template.get("command_poll_question_too_short");
 			}
-			if (!split[1].matches("^\\d$")) {
-				return Template.get("command_poll_time_no_number");
+			String outtext = "A poll has been created by " + author.getName() + Config.EOL + Config.EOL;
+			outtext += "**"+split[0].trim() +"**"+ Config.EOL + Config.EOL;
+			final int answers = Math.min(8, split.length);
+			for (int i = 1; i < answers; i++) {
+				outtext += Misc.numberToEmote(i) + " " + split[i].trim() + Config.EOL + Config.EOL;
 			}
-			int minutes = Integer.parseInt(split[1]);
-			if (minutes <= 0 || minutes > MAX_DURATION_IN_MINUTES) {
-				return Template.get("command_poll_time_out_of_range", 1, MAX_DURATION_IN_MINUTES);
-			}
+			channel.sendMessage(outtext).queue(message -> {
+				for (int i = 1; i < answers; i++) {
+					message.addReaction(Misc.numberToEmote(i)).queue();
+				}
+			});
 		}
 //		return Template.get("command_not_implemented");
 		return "";
