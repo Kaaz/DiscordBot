28d1fefd27d966ed023a04f3c7ccf99d83275091
Kaaz <maik_wezinkhof@hotmail.com>
2016-12-30 11:31:56 +0100

    minor cleanup in !cfg

--- src/main/java/discordbot/command/administrative/SetConfig.java
+++ src/main/java/discordbot/command/administrative/SetConfig.java
@@ -79,79 +79,80 @@ public class SetConfig extends AbstractCommand {
 		}
 
 		if (rank.isAtLeast(SimpleRank.GUILD_ADMIN)) {
-			if (args.length > 0 && args[0].equalsIgnoreCase("reset")) {
-				if (args.length > 1 && args[1].equalsIgnoreCase("yesimsure")) {
-					GuildSettings.get(guild).reset();
-					return Template.get(channel, "command_config_reset_success");
-				}
-				return Template.get(channel, "command_config_reset_warning");
+			return Template.get("command_config_no_permission");
+		}
+		if (args.length > 0 && args[0].equalsIgnoreCase("reset")) {
+			if (args.length > 1 && args[1].equalsIgnoreCase("yesimsure")) {
+				GuildSettings.get(guild).reset();
+				return Template.get(channel, "command_config_reset_success");
 			}
-			if (args.length == 0) {
-				Map<String, String> settings = GuildSettings.get(guild).getSettings();
-				ArrayList<String> keys = new ArrayList<>(settings.keySet());
-				Collections.sort(keys);
-				String ret = "Current Settings for " + guild.getName() + Config.EOL + Config.EOL;
-				ret += ":information_source: Settings indicated with a `*` are different from the default value";
-				List<List<String>> data = new ArrayList<>();
-				for (String key : keys) {
-					if (DefaultGuildSettings.get(key).isReadOnly()) {
-						if (!rank.isAtLeast(SimpleRank.BOT_ADMIN)) {
-							continue;
-						}
-					}
-					List<String> row = new ArrayList<>();
-					String indicator = "";
-					if (rank.isAtLeast(SimpleRank.BOT_ADMIN)) {
-						indicator = DefaultGuildSettings.get(key).isReadOnly() ? "r" : " ";
-					}
-					indicator += settings.get(key).equals(DefaultGuildSettings.getDefault(key)) ? " " : "*";
-					row.add(indicator + key);
-					row.add(settings.get(key));
-					row.add(DefaultGuildSettings.getDefault(key));
-					data.add(row);
-				}
-				List<String> headers = new ArrayList<>();
-				Collections.addAll(headers, "Setting name", "Current", "Default");
-				ret += Misc.makeAsciiTable(headers,
-						data, null);
-				return ret;
-			} else {
-				if (!DefaultGuildSettings.isValidKey(args[0])) {
-					return Template.get("command_config_key_not_exists");
-				}
-				if (DefaultGuildSettings.get(args[0]).isReadOnly() && !rank.isAtLeast(SimpleRank.BOT_ADMIN)) {
-					return Template.get("command_config_key_read_only");
-				}
-				String newValue = "";
-				if (args.length >= 2) {
-					newValue = args[1];
-					for (int i = 2; i < args.length; i++) {
-						newValue += " " + args[i];
-					}
-					if (newValue.length() > 64) {
-						newValue = newValue.substring(0, 64);
+			return Template.get(channel, "command_config_reset_warning");
+		}
+		if (args.length == 0) {
+			Map<String, String> settings = GuildSettings.get(guild).getSettings();
+			ArrayList<String> keys = new ArrayList<>(settings.keySet());
+			Collections.sort(keys);
+			String ret = "Current Settings for " + guild.getName() + Config.EOL + Config.EOL;
+			ret += ":information_source: Settings indicated with a `*` are different from the default value";
+			List<List<String>> data = new ArrayList<>();
+			for (String key : keys) {
+				if (DefaultGuildSettings.get(key).isReadOnly()) {
+					if (!rank.isAtLeast(SimpleRank.BOT_ADMIN)) {
+						continue;
 					}
 				}
-				if (args.length >= 2 && args[0].equals("bot_listen") && args[1].equals("mine")) {
-					channel.sendMessage(Emojibet.WARNING + " I will only listen to the configured `bot_channel`. If you rename the channel, you might not be able to access me anymore. " +
-							"You can reset by typing `@" + channel.getJDA().getSelfUser().getName() + " reset yesimsure`").queue();
+				List<String> row = new ArrayList<>();
+				String indicator = "";
+				if (rank.isAtLeast(SimpleRank.BOT_ADMIN)) {
+					indicator = DefaultGuildSettings.get(key).isReadOnly() ? "r" : " ";
 				}
-				if (args.length >= 2 && GuildSettings.get(guild).set(guild, args[0], newValue)) {
-					bot.getContainer().getShardFor(guild.getId()).clearChannels(guild);
-					return Template.get("command_config_key_modified");
-				}
-				String tblContent = "";
-				GuildSettings setting = GuildSettings.get(guild);
-				for (String s : setting.getDescription(args[0])) {
-					tblContent += s + Config.EOL;
-				}
-				return "Config help for **" + args[0] + "**" + Config.EOL + Config.EOL +
-						"Current value: \"**" + GuildSettings.get(guild).getOrDefault(args[0]) + "**\"" + Config.EOL +
-						"Default value: \"**" + setting.getDefaultValue(args[0]) + "**\"" + Config.EOL + Config.EOL +
-						"Description: " + Config.EOL +
-						Misc.makeTable(tblContent);
+				indicator += settings.get(key).equals(DefaultGuildSettings.getDefault(key)) ? " " : "*";
+				row.add(indicator + key);
+				row.add(settings.get(key));
+				row.add(DefaultGuildSettings.getDefault(key));
+				data.add(row);
+			}
+			List<String> headers = new ArrayList<>();
+			Collections.addAll(headers, "Setting name", "Current", "Default");
+			ret += Misc.makeAsciiTable(headers,
+					data, null);
+			return ret;
+		}
+
+		if (!DefaultGuildSettings.isValidKey(args[0])) {
+			return Template.get("command_config_key_not_exists");
+		}
+		if (DefaultGuildSettings.get(args[0]).isReadOnly() && !rank.isAtLeast(SimpleRank.BOT_ADMIN)) {
+			return Template.get("command_config_key_read_only");
+		}
+
+		if (args.length >= 2) {
+			String newValue = args[1];
+			for (int i = 2; i < args.length; i++) {
+				newValue += " " + args[i];
+			}
+			if (newValue.length() > 64) {
+				newValue = newValue.substring(0, 64);
+			}
+			if (args[0].equals("bot_listen") && args[1].equals("mine")) {
+				channel.sendMessage(Emojibet.WARNING + " I will only listen to the configured `bot_channel`. If you rename the channel, you might not be able to access me anymore. " +
+						"You can reset by typing `@" + channel.getJDA().getSelfUser().getName() + " reset yesimsure`").queue();
+			}
+			if (GuildSettings.get(guild).set(guild, args[0], newValue)) {
+				bot.getContainer().getShardFor(guild.getId()).clearChannels(guild);
+				return Template.get("command_config_key_modified");
 			}
 		}
-		return Template.get("command_config_no_permission");
+
+		String tblContent = "";
+		GuildSettings setting = GuildSettings.get(guild);
+		for (String s : setting.getDescription(args[0])) {
+			tblContent += s + Config.EOL;
+		}
+		return "Config help for **" + args[0] + "**" + Config.EOL + Config.EOL +
+				"Current value: \"**" + GuildSettings.get(guild).getOrDefault(args[0]) + "**\"" + Config.EOL +
+				"Default value: \"**" + setting.getDefaultValue(args[0]) + "**\"" + Config.EOL + Config.EOL +
+				"Description: " + Config.EOL +
+				Misc.makeTable(tblContent);
 	}
 }
