79b9472dc152257b77af1f2fe6b0d97aec11dd9c
Kaaz <maik_wezinkhof@hotmail.com>
2017-01-02 11:57:31 +0100

    override the join-date for a user and an option to reset it

--- src/main/java/discordbot/command/informative/UserCommand.java
+++ src/main/java/discordbot/command/informative/UserCommand.java
@@ -18,6 +18,7 @@ import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
 
 import java.sql.Timestamp;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 /**
@@ -44,8 +45,10 @@ public class UserCommand extends AbstractCommand {
 	@Override
 	public String[] getUsage() {
 		return new String[]{
-				"user         //info about you",
-				"user @user   //info about @user"
+				"user                             //info about you",
+				"user @user                       //info about @user",
+				"user @user joindate yyyy-MM-dd   //overrides the join-date of a user",
+				"user @user joindate reset        //restores the original value"
 		};
 	}
 
@@ -73,44 +76,64 @@ public class UserCommand extends AbstractCommand {
 				infoUser = member.getUser();
 			}
 		}
-		if (infoUser != null) {
-			int userId = CUser.getCachedId(infoUser.getId(), infoUser.getName());
-			int guildId = 0;
-			StringBuilder sb = new StringBuilder();
-			String nickname = infoUser.getName();
-			if (channel instanceof TextChannel) {
-				guildId = CGuild.getCachedId(((TextChannel) channel).getGuild().getId());
-				nickname = ((TextChannel) channel).getGuild().getMember(infoUser).getEffectiveName();
-			}
-			OUser dbUser = CUser.findBy(infoUser.getId());
-			sb.append("Querying for ").append(nickname).append(Config.EOL);
-			sb.append(":bust_in_silhouette: User: ").append(infoUser.getName()).append("#").append(infoUser.getDiscriminator()).append(Config.EOL);
-			sb.append(":id: discord id:").append(infoUser.getId()).append(Config.EOL);
-			sb.append(":keyboard: Commands used:").append(dbUser.commandsUsed).append(Config.EOL);
-			if (guildId > 0) {
-				Guild guild = ((TextChannel) channel).getGuild();
-				OGuildMember member = CGuildMember.findBy(guildId, userId);
-				if (member.joinDate == null) {
-					member.joinDate = new Timestamp(guild.getMember(infoUser).getJoinDate().toInstant().toEpochMilli());
-					CGuildMember.insertOrUpdate(member);
-				}
-
-				sb.append(":date: joined: ")
-						.append(joindateFormat.format(member.joinDate))
-						.append(" (")
-						.append(TimeUtil.getRelativeTime(member.joinDate.getTime() / 1000L, false, true))
-						.append(")")
-						.append(Config.EOL);
-
-			}
-			if (infoUser.getAvatarUrl() != null) {
-				sb.append(":frame_photo: Avatar: <").append(infoUser.getAvatarUrl()).append(">").append(Config.EOL);
-			}
-			if (infoUser.isBot()) {
-				sb.append(":robot: This user is a bot (or pretends to be)");
-			}
-			return sb.toString();
+		if (infoUser == null) {
+			return Template.get("command_user_not_found");
 		}
-		return Template.get("command_user_not_found");
+
+		int userId = CUser.getCachedId(infoUser.getId(), infoUser.getName());
+		int guildId = 0;
+
+		String nickname = infoUser.getName();
+		if (channel instanceof TextChannel) {
+			guildId = CGuild.getCachedId(((TextChannel) channel).getGuild().getId());
+			nickname = ((TextChannel) channel).getGuild().getMember(infoUser).getEffectiveName();
+		}
+		if (args.length >= 3 && guildId > 0) {
+			if (args[1].equals("joindate")) {
+				try {
+					OGuildMember member = CGuildMember.findBy(guildId, userId);
+					Guild guild = ((TextChannel) channel).getGuild();
+					if (args[2].equals("reset")) {
+						member.joinDate = new Timestamp(guild.getMember(infoUser).getJoinDate().toInstant().toEpochMilli());
+					} else {
+						member.joinDate = new Timestamp(joindateFormat.parse(args[2].replace("-", "/")).getTime());
+					}
+					CGuildMember.insertOrUpdate(member);
+					return Template.get("command_user_joindate_set", infoUser.getName(), joindateFormat.format(member.joinDate));
+				} catch (ParseException e) {
+					return Template.get("command_invalid_use");
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		OUser dbUser = CUser.findBy(infoUser.getId());
+		sb.append("Querying for ").append(nickname).append(Config.EOL);
+		sb.append(":bust_in_silhouette: User: ").append(infoUser.getName()).append("#").append(infoUser.getDiscriminator()).append(Config.EOL);
+		sb.append(":id: discord id:").append(infoUser.getId()).append(Config.EOL);
+		sb.append(":keyboard: Commands used:").append(dbUser.commandsUsed).append(Config.EOL);
+		if (guildId > 0) {
+			Guild guild = ((TextChannel) channel).getGuild();
+			OGuildMember member = CGuildMember.findBy(guildId, userId);
+			if (member.joinDate == null) {
+				member.joinDate = new Timestamp(guild.getMember(infoUser).getJoinDate().toInstant().toEpochMilli());
+				CGuildMember.insertOrUpdate(member);
+			}
+
+			sb.append(":date: joined: ")
+					.append(joindateFormat.format(member.joinDate))
+					.append(" (")
+					.append(TimeUtil.getRelativeTime(member.joinDate.getTime() / 1000L, false, true))
+					.append(")")
+					.append(Config.EOL);
+
+		}
+		if (infoUser.getAvatarUrl() != null) {
+			sb.append(":frame_photo: Avatar: <").append(infoUser.getAvatarUrl()).append(">").append(Config.EOL);
+		}
+		if (infoUser.isBot()) {
+			sb.append(":robot: This user is a bot (or pretends to be)");
+		}
+		return sb.toString();
+
 	}
 }
