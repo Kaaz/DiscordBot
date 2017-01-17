b76a72372f520720c755a679ff5931badae30c2e
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2017-01-17 13:47:02 +0100

    added/changed some of the random statussus

--- src/main/java/discordbot/service/BotStatusService.java
+++ src/main/java/discordbot/service/BotStatusService.java
@@ -14,34 +14,43 @@ import java.util.Random;
  */
 public class BotStatusService extends AbstractService {
 	private final static String[] statusList = {
-			"with human pets",
-			"Teaching Minions",
-			"Planking",
-			"Bot simulator 2015",
-			"Pokemon Go",
-			"Cow tipping",
-			"Sorting commands",
-			"Planning for wold domination",
-			"Reading wikipedia",
-			"Talking to Martians",
-			"Homework",
-			"Hearthstone",
-			"Path of exile",
-			"Blackjack",
-			"Half Life 3",
-			"russian roulette",
-			"hide and seek",
+			"with %s human pets",
+			"Teaching %s Minions",
+			"Bot simulator 2%03d",
+			"Pokemon Go, got %s so far",
+			"tipping %s cows",
+			"Sorting commands, what comes after %s",
+			"Planning for wold domination #%s",
+			"Reading wikipedia.org/wiki/%s",
+			"Talking to %s Martians",
+			"Homework | %s assignments",
+			"Hearthstone | rank %s",
+			"Path of exile | level %s",
+			"Blackjack with %s victims",
+			"Half Life %s",
+			"russian roulette | %s left",
+			"hide and seek with %s users",
 			"peekaboo",
-			"\";DROP TABLE",
-			"rating your waifu",
-			"Talking to idiots",
-			"Looking for new jokes",
+			"\";DROP TABLE WHERE id = %s",
+			"rating your waifu a %s",
+			"Talking to %s idiots",
+			"Looking for %s new jokes",
 			"Organizing music",
 			"Trying to remember preferences",
-			"Analyzing fellow humans",
+			"Analyzing %s fellow humans",
 			"Yesterday you said tomorrow",
 			"Let dreams be dreams",
-			"Rare pepe"
+			"finding Rare pepe #%s",
+			"Megaman %s",
+			"Having my %s minutes of fame",
+			"Predicting %s minutes",
+			"Achieving nirvana",
+			"bending spoons, attempt #%s",
+			"Making my top %s most wanted list",
+			"Running %s miles",
+			"Dancing the macarena",
+			"Jousting #%s",
+			"Fishing with %s poles",
 	};
 	private final Random rng;
 
@@ -74,10 +83,10 @@ public class BotStatusService extends AbstractService {
 		int roll = rng.nextInt(100);
 		TextChannel inviteChannel = bot.getShardFor(Config.BOT_GUILD_ID).client.getTextChannelById(Config.BOT_CHANNEL_ID);
 		if (inviteChannel != null && roll < 10) {
-			String fallback = "Feedback @ https://discord.gg/eaywDDt";
+			String fallback = "Feedback @ https://discord.gg/eaywDDt | #%s";
 			inviteChannel.getInvites().queue(invites -> {
 				if (invites != null && !invites.isEmpty()) {
-					setGameOnShards(bot, "Feedback @ https://discord.gg/" + invites.get(0).getCode());
+					setGameOnShards(bot, "Feedback @ https://discord.gg/" + invites.get(0).getCode()+" | %s");
 				} else {
 					setGameOnShards(bot, fallback);
 				}
@@ -85,7 +94,7 @@ public class BotStatusService extends AbstractService {
 
 		} else if (roll < 50) {
 			String username = bot.getShards()[0].client.getSelfUser().getName();
-			setGameOnShards(bot, "@" + username + " help || @" + username + " invite");
+			setGameOnShards(bot, "@" + username + " help | @" + username + " invite | #%s");
 		} else {
 			setGameOnShards(bot, statusList[new Random().nextInt(statusList.length)]);
 		}
@@ -93,7 +102,7 @@ public class BotStatusService extends AbstractService {
 
 	private void setGameOnShards(BotContainer container, String status) {
 		for (DiscordBot shard : container.getShards()) {
-			shard.client.getPresence().setGame(Game.of(status));
+			shard.client.getPresence().setGame(Game.of(String.format(status, shard.getShardId())));
 		}
 	}
 
