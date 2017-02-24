a8d5aa37cba6d88c97ccb94f347b3990ff0dc1dc
Maik Wezinkhof <maik_wezinkhof@hotmail.com>
2017-02-24 09:50:07 +0100

    event manager back to ExecutorService

--- src/main/java/discordbot/event/JDAEventManager.java
+++ src/main/java/discordbot/event/JDAEventManager.java
@@ -25,15 +25,17 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class JDAEventManager implements IEventManager {
     private final BotContainer container;
     private final CopyOnWriteArrayList<EventListener> listeners = new CopyOnWriteArrayList<>();
-//    private final ExecutorService executor;
+    private final ExecutorService executor;
 
     public JDAEventManager(BotContainer container) {
         this.container = container;
-//        executor = Executors.newCachedThreadPool();
+        executor = Executors.newCachedThreadPool();
     }
 
     @Override
@@ -58,18 +60,20 @@ public class JDAEventManager implements IEventManager {
 
     @Override
     public void handle(Event event) {
-//        if (executor.isShutdown()) {
-//            container.reportError(new Exception("NO EVENT MANAGER"), "JDAEventManager", "is kill");
-//            return;
-//        }
-        final List<Object> cachedListeners = getRegisteredListeners();
-        for (Object listener : cachedListeners) {
-            try {
-                ((EventListener) listener).onEvent(event);
-            } catch (Exception e) {
-                container.reportError(e, "JDAEvent", event.getClass().getName());
-                e.printStackTrace();
-            }
+        if (executor.isShutdown()) {
+            container.reportError(new Exception("NO EVENT MANAGER"), "JDAEventManager", "is kill");
+            return;
         }
+        final List<Object> cachedListeners = getRegisteredListeners();
+        executor.submit(() -> {
+            for (Object listener : cachedListeners) {
+                try {
+                    ((EventListener) listener).onEvent(event);
+                } catch (Exception e) {
+                    container.reportError(e, "JDAEvent", event.getClass().getName());
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
